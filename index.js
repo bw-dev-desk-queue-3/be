2d0e1d4ba0c69f6ab48fe16f52b091866c84c098
danielkyman <danny.kyman@gmail.com>
2020-04-26 01:28:56 -0700

    adding endpoints

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require("./api/server.js");
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 6969;
 
 server.listen(port, () => {
   console.log(` server listening on port ${port} - stay fresh`);
---
+++ routes/user-router.js
@@ -0,0 +1,35 @@
+const router = require("express").Router();
+
+const Users = require("../models/users-model.js");
+
+router.get("/", async (req, res) => {
+  try {
+    const users = await Users.find();
+    res.status(201).json(users);
+  } catch (error) {
+    res.status(500).json({ errorMessage: error.message });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await Users.findById(id);
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({ errorMessage: error.message });
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    let newUser = req.body;
+
+    const saved = await Users.add(newUser);
+    res.status(201).json(saved);
+  } catch (error) {
+    res.status(500).json({ errorMessage: error.message });
+  }
+});
+
+module.exports = router;
